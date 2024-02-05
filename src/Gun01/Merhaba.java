b35b5ae724fe92f22e2a7ccf5f37c2a6fd3e8c53
turuhsen <tubaaldemir1013@gmail.com>
2024-02-05 18:39:15 +0100

    satin alma islemi onayi eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,7 +4,11 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
         System.out.println("merhaba 2");
-        System.out.println("merhaba 3");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+
+        }
+
     }
 
 }
